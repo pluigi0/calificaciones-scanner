b32ee6537261093db9899e2f34b8496cf7419e4e
pluig <pluig@LAPTOP-FTUUF4D0>
2021-11-17 15:36:26 -0500

    suma del dinero de 3 personas

--- src/Calificaciones/SumaCalificaciones.java
+++ src/Calificaciones/SumaCalificaciones.java
@@ -16,16 +16,16 @@ public class SumaCalificaciones {
         
         System.out.println("\t\t\tPrograma que calcula e imprime la suma de 3 calificaciones ");
         
-        System.out.println(" Ingrese las 3 calificaciones: "); //leemos las 3 calificaciones
+        System.out.println(" Ingrese las 3 calificaciones: "); //obtenemos las 3 calificaciones
         cali1 = leer.nextDouble();
         System.out.println("\n");
         cali2 = leer.nextDouble();
         System.out.println("\n");
         cali3 = leer.nextDouble();
         
-        suma = cali1 + cali2 +cali3;
+        suma = cali1 + cali2 +cali3; //Sumamos las 3 calificaciones
         
-        System.out.println("\nLa suma de las 3 calificaciones es: "+formato2.format(suma));
+        System.out.println("\nLa suma de las 3 calificaciones es: "+formato2.format(suma)); //Se imprime la suma de las calificaciones
         
     }
 }
