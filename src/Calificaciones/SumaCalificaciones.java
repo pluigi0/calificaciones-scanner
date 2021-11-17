9bd710799d1b2a4b67cb4e996a6cdfae4b481fa7
pluig <pluig@LAPTOP-FTUUF4D0>
2021-11-17 14:53:26 -0500

    Suma de calificaciones con scanner

---
+++ src/Calificaciones/SumaCalificaciones.java
@@ -0,0 +1,31 @@
+
+package Calificaciones;
+
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class SumaCalificaciones {
+    public static void main (String [] args) {
+        Scanner leer = new Scanner (System.in);
+        DecimalFormat formato2 = new DecimalFormat("#.##");
+
+        
+        double cali1, cali2, cali3;
+        
+        double suma;
+        
+        System.out.println("\t\t\tPrograma que calcula e imprime la suma de 3 calificaciones ");
+        
+        System.out.println(" Ingrese las 3 calificaciones: "); //leemos las 3 calificaciones
+        cali1 = leer.nextDouble();
+        System.out.println("\n");
+        cali2 = leer.nextDouble();
+        System.out.println("\n");
+        cali3 = leer.nextDouble();
+        
+        suma = cali1 + cali2 +cali3;
+        
+        System.out.println("\nLa suma de las 3 calificaciones es: "+formato2.format(suma));
+        
+    }
+}
